da89b80dfa86b63a44e33f2e7402a1c4891be97f
AU0758 <a07582709@gmail.com>
2023-12-17 17:13:47 +0000

    Hero section done⭐

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,39 @@
-import Image from 'next/image'
-import { useEffect } from 'react';
+'use client'
+
+
+
+import Image  from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [headphones, setHeadphones] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
-      // You can access scroll information here
-      const scrollY = window.scrollY;
-      console.log('Vertical scroll position: ', scrollY);
+      const scrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 150;
+      setHeadphones(scrollPercentage);
     };
 
-    // Add scroll event listener when the component mounts
     window.addEventListener('scroll', handleScroll);
 
-    // Remove scroll event listener when the component unmounts
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
-  const headphones = {
-    backgroundImage: '@/public/headphones/'+ (Math.round((window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100)) +'.png',
-  }
+  console.log(`/headphone/${Math.round(headphones)}.png`);
 
   return (
-    <main style={headphones}>
-      
+    <main className='h-[100vh] w-[100vw] sticky top-0 flex items-center'>  
+      <img src={`/headphone/${Math.round(headphones)}.png`} alt={`/headphone/${Math.round(headphones)}.png`} className='w-[calc(100vw-7px)] absolute' loading='lazy'/>
+      <div className='w-[100vw] flex flex-col items-end'>
+        <div className='flex flex-col items-center'>
+          <h1 className={`title text-center w-[7ch] m-[5rem] transition-all ${headphones >= 19 ? 'hidden' : ''}`}>IMMERSE IT IN</h1>
+          <p className={`buy bg-[red] w-[15rem] rounded-full  ${headphones >= 19 ? 'hidden' : ''}`}>BUY</p>
+        </div>
+      </div>
+      <p className={` ${headphones <= 16 ? 'hidden' : headphones >= 29 ? '' : 'hidden'}`}>Noise reduction technology for the Best immersion.</p>
     </main>
   )
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[75px] w-[100vw] flex justify-end absolute top-0'>
+        <div className='h-[100%] w-[60%] border-b-[1px] navbar flex items-center justify-end sticky buy'><p className='mr-[20px]'>FAQ</p></div>
+    </div>
+  )
+}
+
+export default Navbar
