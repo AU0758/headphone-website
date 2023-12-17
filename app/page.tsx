2ffae5503373735489b5d8b939083f4e57062f1c
AU0758 <a07582709@gmail.com>
2023-12-17 20:22:17 +0000

    Fixing bugs🕷️

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-
-
 import Image  from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
@@ -25,7 +23,7 @@ export default function Home() {
   console.log(`/headphone/${Math.round(headphones)}.png`);
 
   return (
-    <main className='h-[100vh] w-[100vw] sticky top-0 flex flex-col items-center'>  
+    <main className='h-[100vh] w-[100vw] sticky top-0 flex flex-col justify-center items-center'>  
       <img src={`/headphone/${Math.round(headphones)}.png`} alt={`/headphone/${Math.round(headphones)}.png`} className='w-[calc(100vw-7px)] absolute z-10' loading='lazy'/>
       <div className='w-[100vw] flex flex-col items-end'>
         <div className='flex flex-col items-center'>
@@ -33,10 +31,10 @@ export default function Home() {
           <p className={`buy bg-[red] w-[15rem] rounded-full  ${headphones >= 19 ? 'hidden' : ''}`}>BUY</p>
         </div>
       </div>
-      <p className={` ${headphones <= 16 ? 'hidden' : headphones >= 25 && headphones <= 40 ? 'relative top-[10%] m-24' : 'hidden'}`}><b className='font-300'>Noise reduction technology</b> for the Best immersion.</p>
-      <p className={` ${headphones <= 16 ? 'hidden' : headphones >= 25 && headphones <= 40 ? 'absolute left-0 w-[22ch] text-left m-24' : 'hidden'}`}><b  className='font-300'>Talk</b> instead of typing with our <b  className='font-300'>IA powered</b> assistent. <b  className='font-300'>Just ask.</b></p>
+      <p className={`w-[15ch] ${headphones <= 16 ? 'hidden' : headphones >= 25 && headphones <= 40 ? 'absolute right-0 bottom-[10%] m-24' : 'hidden'}`}><b className='font-300'>Noise reduction technology</b> for the Best immersion.</p>
+      <p className={`w-[18ch] ${headphones <= 16 ? 'hidden' : headphones >= 25 && headphones <= 40 ? 'absolute left-0 w-[22ch] text-left m-24' : 'hidden'}`}><b  className='font-300'>Talk</b> instead of typing with our <b  className='font-300'>IA powered</b> assistent. <b  className='font-300'>Just ask.</b></p>
       
-      <p className={` ${headphones <= 16 ? 'hidden' : headphones >= 50 && headphones <= 70 ? 'relative top-[10%] m-24' : 'hidden'}`}>Revolutionary <b className='font-300'>charging technology</b>. Stay <b className='font-300'>connected</b>.</p>
+      <p className={`w-[21ch] ${headphones <= 16 ? 'hidden' : headphones >= 50 && headphones <= 70 ? 'absolute right-0 bottom-[10%] m-24' : 'hidden'}`}>Revolutionary <b className='font-300'>charging technology</b>. Stay <b className='font-300'>connected</b>.</p>
       <p className={` ${headphones <= 16 ? 'hidden' : headphones >= 50 && headphones <= 70 ? 'absolute top-0 w-[20ch] text-left m-24' : 'hidden'}`}><b className='font-300'>Charge in minutes</b> and <b className='font-300'>use for days.</b></p>
 
       <div className='w-[100vw] flex flex-col items-center'>
